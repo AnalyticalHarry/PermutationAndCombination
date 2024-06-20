aa0ef7ddab16d998ec951df876557adce4cb8af1
Hemant Thapa <104982185+AnalyticalHarry@users.noreply.github.com>
2024-06-20 14:29:04 +0100

    Update 1 CoinToss.js

--- Probability/JavaScript/1 CoinToss.js
+++ Probability/JavaScript/1 CoinToss.js
@@ -1,5 +1,6 @@
 // When throwing a dice twice, what is the probability of getting exactly two 5's?
 
+// function to calculate factorial
 const factorial = (n) => {
     if (n === 0 || n === 1) {
         return 1;
@@ -11,18 +12,23 @@ const factorial = (n) => {
     return result;
 };
 
+// function to calcualte binomial coefficient 
 const binomial_coefficient = (n, k) => {
     return factorial(n) / (factorial(k) * factorial(n - k));
 };
 
+// function to calculate probability
 const probability_exact_k_outcomes = (n, k, p) => {
     let coeff = binomial_coefficient(n, k);
     let probability = coeff * (p ** k) * ((1 - p) ** (n - k));
     return probability;
 };
 
+// number of rolls
 let n = 2;
+// number of specific outcoms
 let k = 2;
+// probability of rolling a 5 on a single roll
 let p = 1/6; 
 let prob = probability_exact_k_outcomes(n, k, p)  
 console.log(`The probability of rolling exactly {k} 5's in {n} rolls is ${prob}`)
